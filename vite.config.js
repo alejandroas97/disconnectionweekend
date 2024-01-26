8b382d138461c10b556bd0716dc2e9b7bd921514
alejandroas97 <93349487+alejandroas97@users.noreply.github.com>
2024-01-26 10:20:29 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,6 @@ export default defineConfig(() => {
                 '@': fileURLToPath(new URL('./src', import.meta.url))
             }
         },
-        base: "/disconnection-weekend/"
+        base: "https://alejandroas97.github.io/disconnectionweekend/"
     };
 });
