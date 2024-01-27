ee626144cf9e0381fea636c43f2b56c46a25db8a
alejandro <aleas97@outlook.com>
2024-01-27 01:10:12 +0000

    Landing

--- src/views/pages/Landing.vue
+++ src/views/pages/Landing.vue
@@ -29,20 +29,20 @@ const logoUrl = computed(() => {
             </div> -->
             <div
                 id="hero"
-                class="flex flex-column pt-4 px-4 lg:px-8 overflow-hidden"
-                style="background: linear-gradient(0deg, rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.2)), radial-gradient(77.36% 256.97% at 77.36% 57.52%, rgb(238, 239, 175) 0%, rgb(195, 227, 250) 100%); clip-path: ellipse(150% 87% at 93% 13%)"
+                class="flex h.10 flex-column pt-4 px-4 lg:px-8 overflow-hidden"
+                style="height: 1000px; background: linear-gradient(0deg, rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.2)), radial-gradient(77.36% 256.97% at 77.36% 57.52%, rgb(238, 239, 175) 0%, rgb(195, 227, 250) 100%); clip-path: ellipse(150% 87% at 93% 13%)"
             >
                 <div class="mx-4 md:mx-8 mt-0 md:mt-4">
-                    <h1 class="text-6xl font-bold text-gray-900 line-height-2"><span class="font-light block">Enhorabuena!!</span>Has pasado el reto</h1>
-                    <p class="font-normal text-2xl line-height-3 md:mt-3 text-gray-700">Sed blandit libero volutpat sed cras. Fames ac turpis egestas integer. Placerat in egestas erat...</p>
+                    <h1 class="text-6xl font-bold text-gray-900 line-height-2"><span class="font-light block">Enhorabuena!!</span>Has pasado el primer reto</h1>
+                    <p class="font-normal text-2xl line-height-3 md:mt-3 text-gray-700">Toca en el siguiente enlace para obtener los días del evento.</p>
                     <Button label="Añadir a mi calendario" @click="redirectToAnotherPage" />
                 </div>
-                <div class="flex justify-content-center md:justify-content-end">
+                <!-- <div class="flex justify-content-center md:justify-content-end">
                     <img src="/demo/images/landing/screen-1.png" alt="Hero Image" class="w-9 md:w-auto" />
-                </div>
+                </div> -->
             </div>
 
-            <div id="features" class="py-4 px-4 lg:px-8 mt-5 mx-0 lg:mx-8">
+            <!-- <div id="features" class="py-4 px-4 lg:px-8 mt-5 mx-0 lg:mx-8">
                 <div class="grid justify-content-center">
                     <div class="col-12 text-center mt-8 mb-4">
                         <h2 class="text-900 font-normal mb-2">Marvelous Features</h2>
@@ -376,7 +376,7 @@ const logoUrl = computed(() => {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
     <AppConfig simple />
